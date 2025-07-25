a3f06c20b1a72cd46e41b884aeb48e8e1b6accf4
himanshu-2l <himanshurathore212.2l@gmail.com>
2025-07-25 06:10:03 -0700

    Delete src directory
    delete

--- src/app/metadata.ts
+++
@@ -1,6 +0,0 @@
-import type { Metadata } from "next"
-
-export const metadata: Metadata = {
-  title: "RecycleX",
-  description: "AI-powered waste management platform",
-}
--- src/scripts/createSchemas.js
+++
@@ -1,8 +0,0 @@
-import { createSchemas } from '../utils/signSchemas';
-
-const run = async () => {
-  const schemaIds = await createSchemas();
-  console.log('Schema IDs:', schemaIds);
-};
-
-run().catch(console.error);
